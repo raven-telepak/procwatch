1e4294c7011446cb681679257580a3474165d9cd
Raven.Telepak <rtelepak25@gmail.com>
2026-05-29 10:19:24 -0400

    flag suspicious paths v1, cleaned up test blocks

--- procwatch.py
+++ procwatch.py
@@ -1,6 +1,16 @@
 import psutil
 import os
 
+TRUSTED_PATHS = [
+    "/usr/",
+    "/bin/",
+    "/sbin/",
+    "/System/",
+    "/Library/",
+    "/Applications/",
+    "/private/",
+]
+
 def enumerate_processes():
     process_list = [] #empty list for proc results
 
@@ -14,15 +24,9 @@ def enumerate_processes():
                 "memory_mb" : (proc.memory_info().rss/(1024 * 1024))
             }
             process_list.append(proc_dict) #adds each proc dict to list
-        except Exception:
-            continue #skip procs that cant be accessed
+        except Exception: continue #skip procs that cant be accessed
 
     return process_list #brings compiled list back
-
-#if __name__ == "__main__": #test enumerate_processes
-#    processes = enumerate_processes()
-#    for p in processes:
-#        print(p)
 
 def detect_ghost_processes(process_list):
     ghost_processes = []
@@ -30,16 +34,35 @@ def detect_ghost_processes(process_list):
     for proc in process_list:
         exe = proc["exe"]
 
-        if not exe: #skip procs with no path
-            continue
+        if not exe: continue #skip procs with no path
 
         if not os.path.exists(exe):  # path doesn't exist on disk
             ghost_processes.append(proc)
 
     return ghost_processes
 
+def flag_suspicious_paths(process_list):
+    suspicious = []
 
-#if __name__ == "__main__": #tests detect_ghost_processes
+    for proc in process_list:
+        exe = proc["exe"]
+
+        if not exe: continue
+
+        if not any(exe.startswith(path) for path in TRUSTED_PATHS):
+            suspicious.append(proc)
+
+    return suspicious
+
+
+#test block
+
+# if __name__ == "__main__": #test enumerate_processes
+#    processes = enumerate_processes()
+#    for p in processes:
+#        print(p)
+
+# if __name__ == "__main__": #tests detect_ghost_processes
 #   processes = enumerate_processes()
 #   ghosts = detect_ghost_processes(processes)
 #   print(f"\nGhost processes found: {len(ghosts)}")
